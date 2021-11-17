25f19a6edb5f56e48de33d41198d270ce8c0f37c
Yuriy Martini <yuriy.martini@live.it>
2021-11-17 02:06:05 +0100

    fix

--- src/classes/stockx.js
+++ src/classes/stockx.js
@@ -22,7 +22,7 @@ module.exports = class StockX {
      * @param options.currency - The currency to make requests in
      */
     constructor(options = {}){
-        const { proxy, country, currency, userAgent } = options;
+        const { proxy, country, currency, timeout, userAgent } = options;
 
         this.userAgent = userAgent || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36';
         this.country = country || 'US';
